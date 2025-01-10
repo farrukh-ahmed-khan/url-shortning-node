e693057e757fac216afeb76ed7e5548b90c9ad5d
Farrukh Ahmed Khan <77009721+farrukh-ahmed-khan@users.noreply.github.com>
2025-01-10 23:42:34 +0500

    fixes-on-statefull-authentication

--- controllers/user.js
+++ controllers/user.js
@@ -19,9 +19,9 @@ async function handleUserLogin(req, res) {
       error: "invalid username or password",
     });
   }
-  const sessionId = uuidv4();
-  setUser(sessionId, user)
-  res.cookie("uid", sessionId)
+  
+  const token = setUser(user);
+  res.cookie("uid", token)
   return res.redirect("/")
 
   // return res.status(201).json({message: "User created"});
